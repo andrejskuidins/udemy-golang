87a1688d22b71d7f6f2aabe3ee081723f33725fa
Andrejs Kuidins <andrejs.kuidins@1nce.com>
2023-10-25 17:05:35 +0300

    master

--- lectures/exercise/sr-dice/dice.go
+++ lectures/exercise/sr-dice/dice.go
@@ -23,7 +23,34 @@
 
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func diceRoll(sides int) int {
+  rand.Seed(time.Now().UnixNano())
+  dice := rand.Intn(sides) + 1
+  return dice
+}
 
 func main() {
+  diceRollResult := 0
+  diceRollCount := 2
+  diceRollSides := 7
+  for i := 1; i <= diceRollCount; i++ {
+    diceRollResult = diceRoll(diceRollSides)
+  }
+  if diceRollCount == 2 && diceRollResult == 2 {
+    fmt.Println("Snake eyes")
+  } else if diceRollResult == 7 {
+    fmt.Println("Lucky 7")
+  } else if diceRollResult % 2 == 0 {
+    fmt.Println("Even")
+  } else if diceRollResult % 2 != 0 {
+    fmt.Println("Odd")
+  }
+  fmt.Println(diceRollCount)
+  fmt.Println(diceRollResult)
 }
